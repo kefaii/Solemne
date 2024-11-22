3988057da444e12aec9e1dfeb323d0c419be2998
kefaii <hanstroncosopalma@gmail.com>
2024-11-22 17:35:50 -0300

    Update codex1.py

--- codex1.py
+++ codex1.py
@@ -82,7 +82,7 @@ if response.status_code == 200:
     # Crear botones para ordenar columnas
     
     def ascendente(df):
-        df = df.sort_values(by='columna', ascending=True)
+        df = df.sort_values(by=columna_estadistica, ascending=True)
         return st.write(df)
         
     st.button("ascendente", on_click=ascendente(df))
